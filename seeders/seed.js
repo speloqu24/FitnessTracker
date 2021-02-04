4261f1bdff3fdad580e77d1a9f4e35e066e9c7d5
Speloquin <speloqu24@gmail.com>
2021-02-04 12:29:01 -0700

    seed connection

--- seeders/seed.js
+++ seeders/seed.js
@@ -4,6 +4,7 @@ let db = require("../models");
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
+  useUnifiedTopology: true,
 });
 
 let workoutSeed = [
--- server.js
+++ server.js
@@ -14,9 +14,17 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false,
+// });
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
+  useUnifiedTopology: true,
 });
 
 app.use(require("./routes/html-routes"));
