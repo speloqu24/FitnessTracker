b30d11b4b08615f95c6307a90d6121f77c6fbe41
Speloquin <speloqu24@gmail.com>
2021-02-04 18:25:14 -0700

    deploying to Mongo and heroku

--- seeders/seed.js
+++ seeders/seed.js
@@ -4,6 +4,7 @@ let db = require("../models");
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
+  useCreateIndex: true,
   useUnifiedTopology: true,
 });
 
